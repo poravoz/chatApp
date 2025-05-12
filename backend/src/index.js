5c4846bcb15a394ab2234e562fb5b0d3962ed700
poravoz <116353344+poravoz@users.noreply.github.com>
2025-05-13 01:32:43 +0300

    ыуа

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,13 +33,14 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+/*
 if(process.env.NODE_ENV==="production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   })
-}
+} */
 
 connectDB().then(() => {
   server.listen(PORT, () => {
